acf6bcf17dcd4ebc203ffd5884bb553f7c50a714
Ankur7891 <ankurmajumdar7891@gmail.com>
2024-01-12 21:41:16 +0530

    2nd Draft FrontEnd

--- FrontEnd/src/Sections/Inputs/Inputs.jsx
+++ FrontEnd/src/Sections/Inputs/Inputs.jsx
@@ -3,30 +3,36 @@ import styles from './Inputs.module.css';
 import TextBox from '../../Components/TextBox/TextBox';
 
 function Inputs() {
-    const exportData = () => {
+    const divertClient = () => {
 
     };
   return (
-    <form id="gdsc-attendance" action="#" method="post" onSubmit={exportData}>
-      <TextBox
-        name="Name"
-        type="text"
-        placeholder="Enter Your Name"
-        isRequired="true"
-      />
-      <TextBox
-        name="Roll No."
-        type="text"
-        placeholder="Enter Your Roll Number"
-        isRequired="true"
-      />
-      <TextBox
-        name="Mobile No."
-        type="text"
-        placeholder="Enter Your Mobile Number"
-        isRequired="true"
-      />
-      <button type="submit" className={styles.SubmitBtn}> Submit </button>
+    <form id="gdsc-attendance" action="#" method="post" onSubmit={divertClient}>
+      <div className={styles.FormContent}>
+        <TextBox
+          name="Name"
+          type="text"
+          placeholder="Enter Your Name"
+          isRequired="true"
+        />
+        <TextBox
+          name="Roll No."
+          type="text"
+          placeholder="Enter Your Roll Number"
+          isRequired="true"
+        />
+        <TextBox
+          name="Mobile No."
+          type="text"
+          placeholder="Enter Your Mobile Number"
+          isRequired="true"
+        />
+        <div className={styles.BtnContainer}>
+          <button type="submit" className={styles.SubmitBtn}>
+            Submit
+          </button>
+        </div>
+      </div>
     </form>
   );
 }
